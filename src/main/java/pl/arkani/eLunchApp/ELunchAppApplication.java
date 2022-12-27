0e041527d06996def99daafdd957cdab2477d0bb
komp <komp@kali.komp>
2022-12-27 21:18:00 +0100

    Initial commit

---
+++ src/main/java/pl/arkani/eLunchApp/ELunchAppApplication.java
@@ -0,0 +1,13 @@
+package pl.arkani.eLunchApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ELunchAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ELunchAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/arkani/eLunchApp/ELunchAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.arkani.eLunchApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ELunchAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
